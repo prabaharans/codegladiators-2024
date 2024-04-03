5cbd1080c12b2e35cdaf1c7b85fc9b773167749e
Prabaharan <spraba84@gmail.com>
2024-04-04 03:36:11 +0530

    updated

--- problem-one-new.php
+++ problem-one-new.php
@@ -5,14 +5,14 @@ $T = readline();
 while ($T--) {
     // Input for Paul
     list($P, $X, $R1) = explode(' ', readline());
-    
+
     // Input for Nina
     list($N, $Y, $R2) = explode(' ', readline());
-    
+
     // Calculate time taken by Paul and Nina
     $timePaul = $P / $X + $R1;
     $timeNina = $N / $Y + $R2;
-    
+
     // Compare times and output the result
     if ($timePaul < $timeNina) {
         echo "PAUL\n" . (int)$timePaul . "\n";
